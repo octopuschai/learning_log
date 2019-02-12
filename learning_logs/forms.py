a6cb2d1a6f1ec0bc50bb7da23a97a12ce5dccb06
oct <n@n.com>
2019-02-12 21:59:25 +0800

    bulid learning_log repository

---
+++ learning_logs/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Topic, Entry
+
+class TopicForm(forms.ModelForm):
+    '''主题表单类'''
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text':''}
+
+class EntryForm(forms.ModelForm):
+    '''条目表单类'''
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text':''}
+        widgets = {'text':forms.Textarea(attrs={'cols':80})}
