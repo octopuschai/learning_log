dbbcbb683d3a2af7d0cd2fdd974608f60ba896af
Jones <chaiy2004@163.com>
2019-02-14 16:15:01 +0800

    fixed STATIC_ROOT error

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -151,7 +151,8 @@ if os.environ['HOME'] == "/app":
     # 静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     # 书中设置是这样的： STATIC_ROOT = "staticfiles"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static"),
     )
